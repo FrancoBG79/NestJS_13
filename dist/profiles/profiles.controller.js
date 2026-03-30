4fa71c86cbcd152e7f51255e81b55821e1bb55df
FrancoBG79 <fbg2468@gmail.com>
2026-03-30 10:27:43 +0200

    add profiles controller

--- dist/profiles/profiles.controller.js
+++ dist/profiles/profiles.controller.js
@@ -6,19 +6,12 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppModule = void 0;
+exports.ProfilesController = void 0;
 const common_1 = require("@nestjs/common");
-const app_controller_1 = require("./app.controller");
-const app_service_1 = require("./app.service");
-const profiles_module_1 = require("./profiles/profiles.module");
-let AppModule = class AppModule {
+let ProfilesController = class ProfilesController {
 };
-exports.AppModule = AppModule;
-exports.AppModule = AppModule = __decorate([
-    (0, common_1.Module)({
-        imports: [profiles_module_1.ProfilesModule],
-        controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
-    })
-], AppModule);
-//# sourceMappingURL=app.module.js.map
+exports.ProfilesController = ProfilesController;
+exports.ProfilesController = ProfilesController = __decorate([
+    (0, common_1.Controller)('profiles')
+], ProfilesController);
+//# sourceMappingURL=profiles.controller.js.map
